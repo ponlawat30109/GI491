0d2fbd4ee6cb96647cdee7b089a7840ed680df63
ponlawat30109 <ponlawat30109@gmail.com>
2021-06-25 08:43:52 +0700

    Update movement

--- Maze/Assets/Scripts/CharMovement.cs
+++ Maze/Assets/Scripts/CharMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CharMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] CharacterController characterController;
+    private Vector3 gravity;
+    private Vector3 playerVelocity;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+
+    void Awake()
     {
-        
+        // characterController = GetComponent<CharacterController>();
+        gravity = Physics.gravity;
+        // playerVelocity.y = 3;
+        // Debug.Log(gravity);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!characterController.isGrounded)
+        {
+            characterController.Move(gravity * Time.deltaTime);
+            Debug.Log("Player touch ground");
+            // playerVelocity.y = 0;
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(movement * Time.deltaTime * playerSpeed);
+
+        // else
+        // {
+        //     if (Input.GetButtonDown("Jump"))
+        //     {
+        //         Debug.Log("Player jump");
+        //         playerVelocity.y += Mathf.Sqrt(5 * -3f * gravity.y);
+        //         characterController.Move(playerVelocity * Time.deltaTime);
+        //     }
+        // }
     }
 }
