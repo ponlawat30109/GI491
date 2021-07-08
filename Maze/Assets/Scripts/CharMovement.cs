046a04d86fe0a8f9e035c112ae70080a1417dc3d
Saruntun_Yimyoo <saruntun.yimy@bumail.net>
2021-07-08 22:16:48 +0700

    COM Animation walk

--- Maze/Assets/Scripts/CharMovement.cs
+++ Maze/Assets/Scripts/CharMovement.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharMovement : MonoBehaviour
 {
-    [SerializeField] CharacterController characterController;
-    private Vector3 gravity;
+    [SerializeField] CharacterController characterController; 
+    //private Vector3 gravityVector3;
     private Vector3 playerVelocity;
+    private Vector3 movement;
+    private Vector3 velocity;
+    private Animator animator;
+    private float horizontal;
+    private float vertical;
+    private float gravity;
+    
     [SerializeField] float playerSpeed;
     [SerializeField] float jumpHeight;
 
@@ -15,25 +23,27 @@ public class CharMovement : MonoBehaviour
     [SerializeField] float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
     
-    Animator animator;
-
     void Awake()
     { 
         //characterController = GetComponent<CharacterController>();
-        gravity = Physics.gravity;
+        //gravityVector3 = Physics.gravity;
         // playerVelocity.y = 3;
         // Debug.Log(gravity);
     }
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
+        animator = transform.GetChild(0).GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
+
+        gravity = 0.5f;
     }
 
     void Update()
     {
-        Movement();
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
         // if (!characterController.isGrounded)
         // {
@@ -56,12 +66,54 @@ public class CharMovement : MonoBehaviour
         // }
     }
 
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
     void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(horizontal, 0, vertical).normalized;
+        
+        //movement = characterController.transform.forward * vertical;
+        
+        //characterController.transform.Rotate(Vector3.up * horizontal * (100f * Time.deltaTime));
 
-        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+        //characterController.Move(movement * playerSpeed * Time.deltaTime);
+        
+        
+        if (characterController.isGrounded)
+        {
+            velocity.y = 0f;
+        }
+        else
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        characterController.Move(velocity);
+
+        // Animation
+        if (horizontal == 0 && vertical ==0)
+        {
+            animator.SetBool("Run",false);
+            animator.SetBool("Reverse", false);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetBool("Reverse", true);
+            animator.SetBool("Run", false);
+        }
+        else if (vertical > 0)
+        {
+            animator.SetBool("Reverse", false);
+            animator.SetBool("Run",true);
+        }
+        else
+        {
+            animator.SetBool("Reverse", false);
+            animator.SetBool("Run",true);
+        }
+        
         // Debug.Log(movement.magnitude);
         if (movement.magnitude >= 0.1f)
         {
@@ -73,14 +125,11 @@ public class CharMovement : MonoBehaviour
             Vector3 moveDir = Vector3.zero;
             if (vertical >= 0)
             {
-                animator.SetBool("Run",true);
-                animator.SetBool("Reverse",false);
                 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
             }
             else
             {
-                animator.SetBool("Reverse",true);
-                animator.SetBool("Run",false);
+                //animator.SetBool("Reverse",false);
                 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.back;
             }
 
